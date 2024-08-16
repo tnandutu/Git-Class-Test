e29b2cf1644209cfdc43cdd1c6240a5c306c1786
Teddy Nandutu <nandutut2013@gmail.com>
2024-08-16 19:47:51 +0300

    update

--- git.js
+++ git.js
@@ -1,2 +1,9 @@
-console.log("Hello world");
+//console.log("Hello world");//
+var string1 = "123";
+var intValue = 123;
+alert(string1 + intValue);
 
+int a == 2;
+int b = 4;
+int ans = a + b;
+print(ans);
